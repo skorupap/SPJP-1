c9a8280d53d2386ea624ddf850ebd655b47b9c8b
Paweł Skorupa <p.skorupa.613@studms.ug.edu.pl>
2022-11-20 09:21:34 +0100

    Zadanie 1

---
+++ Lab4/Zadanie1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    int a,b;
+    printf("Wpisz liczbe: ");
+    scanf("%d",&a);
+    for (int i = 1; i<=a; i++){
+        b += i*i;        
+    }
+    printf("Suma kwadratow wynosi: %d", b);
+}
