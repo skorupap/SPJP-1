3eac7ddf928b4203920d6c418432205b62ae3a21
Paweł Skorupa <p.skorupa.613@studms.ug.edu.pl>
2022-12-18 11:17:54 +0100

    Zadanie 2 (Lab7)

---
+++ Lab7/Zadanie2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int tab[10];
+
+    printf("Wprowadz 10 liczb: \n");
+    for (int i=0; i<10; i++) {
+        scanf("%d", &tab[i]);
+    }
+
+    printf("Elementy tablicy: \n");
+    for (int i=0; i<10; i++) {
+        int * p = &tab[i];
+        printf("%d ", * p);
+    }
+    
+    printf("\n");
+
+}
